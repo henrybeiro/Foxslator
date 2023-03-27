8ef8db88877b98c037f48a4a802607f90957a336
henrybeiro <henryribe18@gmail.com>
2023-03-27 00:27:34 -0300

    snapshot

---
+++ Foxslator/src/main/java/Interfaces/Foxslator.java
@@ -0,0 +1,19 @@
+package Interfaces;
+import java.io.IOException;
+
+/* O Foxslator é o nosso projeto final da disciplina INF01120 - Técnicas de Construção de  	Programas
+do Instituto de Informática (INF-UFRGS) no semestre letivo de 2022/2.
+-> Grupo:
+Luana Taynara da Costa Hahn: luanatc.hahn@gmail.com
+Henry Ribeiro Piceni: henryribe18@gmail.com
+Eduardo Aurelio Halmenschlager Duarte: eduardohduarte@gmail.com
+Luis Filipe Martini Gastmann: Lipe.martini@hotmail.com
+ */
+
+public class Foxslator {
+
+    public static void main(String[] args) throws IOException {
+        new FoxslatorInit().setVisible(true);
+        
+    }
+}
---
+++ Foxslator/src/main/java/TextConverter/JFugueMusicDownloader.java
@@ -0,0 +1,50 @@
+package TextConverter;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import org.jfugue.player.Player;
+
+public class JFugueMusicDownloader {
+    private String music;
+    private String fileName;
+    private String directoryPath;
+
+
+    public JFugueMusicDownloader(String music, String fileName, String directoryPath, String downloadsDirectoryPath) {
+        this.music = music;
+        //System.out.println(this.fileName);
+        if (fileName == "") {
+            this.fileName = "minha_musica.mid";
+        } else {
+                this.fileName = fileName + ".mid";
+            }
+        //System.out.println(directoryPath);
+        //System.out.println(downloadsDirectoryPath);
+
+            if (directoryPath == "") {
+                this.directoryPath = downloadsDirectoryPath;
+            } else {
+                this.directoryPath = directoryPath;
+            }
+        //System.out.println(this.directoryPath);
+        }
+
+
+        public void downloadMusic() {
+
+            Player player = new Player();
+            File file = new File(directoryPath + "/" + fileName);
+
+            try {
+                FileOutputStream fos = new FileOutputStream(file);
+                org.jfugue.pattern.Pattern pat = new org.jfugue.pattern.Pattern();
+                pat.add(music);
+                org.jfugue.midi.MidiFileManager.savePatternToMidi(pat, fos);
+                fos.close();
+                System.out.println("Música salva com sucesso em " + directoryPath + "/" + fileName);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
